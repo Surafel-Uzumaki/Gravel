82de01c3c424a91cd1d860ad3d24fd5c861e3b5a
beaman <beamanbelay@gmail.com>
2024-04-06 11:52:57 +0300

    case

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -2,67 +2,80 @@ import React from "react";
 
 const CaseStudy = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-black mb-8">Case Study</h1>
-      <div className=" gap-6 flex flex-col md:flex-row">
-        <div className="border border-brown-600 rounded-md">
-          <h2 className="text-lg font-semibold text-gray-800 mb-4">Category 1</h2>
-          <div className="ml-4 relative">
-              <video controls className="w-full" height="200" style={{ borderRadius: '8px' }}>
+    <>
+      <div className="container mx-auto px-6 py-8 w-full">
+        <h1 className="text-3xl font-bold text-black mb-8">Case Study</h1>
+        <div className="flex flex-col md:flex-row justify-center md:justify-start gap-6">
+          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4">
+            <a href="#!">
+              <video
+                width="320"
+                height="240"
+                controls
+                className="w-full rounded-t-lg"
+              >
                 <source src="./videos/peter.mp4" type="video/mp4" />
+                <source src="movie.ogg" type="video/ogg" />
                 Your browser does not support the video tag.
               </video>
-              <div className="absolute top-0 left-0 w-full h-full border-brown-600 border-4 rounded-md" style={{ zIndex: '-1' }}></div>
-          <div className="rounded-md bg-brown-50 p-4  mt-5 flex items-center">
-         
-  <p className="text-brown-800 mt-8 max-w-full">Text askdaskjlfasifjaskfljasjkflasfjnascasjncasklfnaslfkujas goes here.</p>
-
-
-            
+            </a>
+            <div className="p-6 text-brown-800">
+              <h5 className="text-xl font-medium mb-2">Video Card</h5>
+              <p className="text-base mb-2">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
             </div>
           </div>
-        </div>
-        
-        <div className="border border-brown-600 rounded-md">
-          <h2 className="text-lg font-semibold text-gray-800 mb-4">Category 1</h2>
-          <div className="relative items-center justify-center flex flex-col">
-              <video controls className="w-[98%] ml-1 sm:h-auto sm:w-[80%] " height="200"  style={{ borderRadius: '8px' }}>
+
+          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4">
+            <a href="#!">
+              <video
+                width="320"
+                height="240"
+                controls
+                className="w-full rounded-t-lg"
+              >
                 <source src="./videos/peter.mp4" type="video/mp4" />
+                <source src="movie.ogg" type="video/ogg" />
                 Your browser does not support the video tag.
               </video>
-              <div className="absolute top-0 left-0 w-full h-full border-brown-600 border-4 rounded-md" style={{ zIndex: '-1' }}></div>
-          <div className="rounded-md bg-brown-50 p-4 flex items-center">
-          <div className=" flex">
-  <p className="text-brown-800 mt-4 text-center justify-center">Text askdaskjlfasifjaskfljasjkflasfjnascasjncasklfnaslfkujas goes here.</p>
-</div>
-
-            
+            </a>
+            <div className="p-6 text-brown-800">
+              <h5 className="text-xl font-medium mb-2">Video Card</h5>
+              <p className="text-base mb-4">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
             </div>
           </div>
-        </div>
-        {/* --------------------- */}
-        <div className="border border-brown-600 rounded-md">
-          <h2 className="text-lg font-semibold text-gray-800 mb-4">Category 1</h2>
-          <div className="relative items-center justify-center">
-              <video controls className="w-[98%] ml-1 "  style={{ borderRadius: '8px' }}>
+
+          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4">
+            <a href="#!">
+              <video
+                width="320"
+                height="240"
+                controls
+                className="w-full rounded-t-lg"
+              >
                 <source src="./videos/peter.mp4" type="video/mp4" />
+                <source src="movie.ogg" type="video/ogg" />
                 Your browser does not support the video tag.
               </video>
-              <div className="absolute top-0 left-0 w-full h-full border-brown-600 border-4 rounded-md" style={{ zIndex: '-1' }}></div>
-          <div className="rounded-md bg-brown-50 p-4 flex items-center">
-          <div className=" flex">
-  <p className="text-brown-800 mt-4 text-center justify-center">Text askdaskjlfasifjaskfljasjkflasfjnascasjncasklfnaslfkujas goes here.
-  sdfakjslfasfkasjlfka;sjfkasjfaskfjaskjfasklncasjlcn asfkjlnasfkjlasnf asfasnfaskjlnf</p>
-</div>
-
-            
+            </a>
+            <div className="p-6 text-brown-800">
+              <h5 className="text-xl font-medium mb-2">Video Card</h5>
+              <p className="text-base mb-4">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
             </div>
           </div>
-        </div>
 
-        {/* Add similar code for other categories */}
+          {/* Add similar code for other categories */}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/Whatwedo.js
+++ src/components/Whatwedo.js
@@ -98,7 +98,7 @@ const Workdone = () => {
           <div className="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0 xl:justify-start">
             <a
               href="#"
-              className="flex items-center justify-center gap-x-2 py-2 px-4 text-white font-medium bg-yellow-500 duration-150 hover:bg-yellow-700 active:bg-gray-900 rounded-lg md:inline-flex font-mono"
+              className="flex items-center justify-center gap-x-2 py-2 px-4 text-white font-medium bg-brown-500 duration-150 hover:bg-yellow-700 active:bg-gray-900 rounded-lg md:inline-flex font-mono"
             >
               Lorem ipsum or social buttons
               
