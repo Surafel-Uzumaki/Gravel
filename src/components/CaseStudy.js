c6dff9161e9e8b8b3c8c88c3ef1a1510c4ae4532
Surafel-uzumaki <smengistu4@gmail.com>
2024-04-08 21:08:27 +0300

    case study updated

--- src/components/CaseStudy.js
+++ src/components/CaseStudy.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CaseStudy = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  const handleShowMore = () => {
+    setShowMore(true);
+  };
+
+  const handleShowLess = () => {
+    setShowMore(false);
+  };
+
   return (
     <>
-      <div className="container mx-auto px-6 py-8 w-full">
-        <h1 className="text-3xl font-bold text-black mb-8">Case Study</h1>
-        <div className="flex flex-col md:flex-row justify-center md:justify-start gap-6">
-          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4">
+      <div className="container lg:mx-auto lg:px-6 lg:py-8 w-full">
+        <h1 className="text-3xl font-bold text-black mb-8 mt-10">Case Study</h1>
+        <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-6 justify-center justify-items-center">
+          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4 ">
             <a href="#!">
               <video
                 width="320"
@@ -20,7 +30,7 @@ const CaseStudy = () => {
               </video>
             </a>
             <div className="p-6 text-brown-800">
-              <h5 className="text-xl font-medium mb-2">Video Card</h5>
+              <h5 className="text-xl font-medium mb-2">Video Card 1</h5>
               <p className="text-base mb-2">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -28,7 +38,7 @@ const CaseStudy = () => {
             </div>
           </div>
 
-          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4">
+          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4 flex-grow">
             <a href="#!">
               <video
                 width="320"
@@ -42,7 +52,7 @@ const CaseStudy = () => {
               </video>
             </a>
             <div className="p-6 text-brown-800">
-              <h5 className="text-xl font-medium mb-2">Video Card</h5>
+              <h5 className="text-xl font-medium mb-2">Video Card 2</h5>
               <p className="text-base mb-4">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -50,7 +60,7 @@ const CaseStudy = () => {
             </div>
           </div>
 
-          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4">
+          <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4 flex-grow">
             <a href="#!">
               <video
                 width="320"
@@ -64,7 +74,7 @@ const CaseStudy = () => {
               </video>
             </a>
             <div className="p-6 text-brown-800">
-              <h5 className="text-xl font-medium mb-2">Video Card</h5>
+              <h5 className="text-xl font-medium mb-2">Video Card 3</h5>
               <p className="text-base mb-4">
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
@@ -72,8 +82,76 @@ const CaseStudy = () => {
             </div>
           </div>
 
-          {/* Add similar code for other categories */}
+          {showMore && (
+            <>
+              <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4 flex-grow">
+                <a href="#!">
+                  <video
+                    width="320"
+                    height="240"
+                    controls
+                    className="w-full rounded-t-lg"
+                  >
+                    <source src="./videos/peter.mp4" type="video/mp4" />
+                    <source src="movie.ogg" type="video/ogg" />
+                    Your browser does not support the video tag.
+                  </video>
+                </a>
+                <div className="p-6 text-brown-800">
+                  <h5 className="text-xl font-medium mb-2">Video Card 4</h5>
+                  <p className="text-base mb-2">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+
+              <div className="rounded-lg shadow-lg bg-brown-50 max-w-sm w-full md:w-auto m-10 md:m-4 flex-grow">
+                <a href="#!">
+                  <video
+                    width="320"
+                    height="240"
+                    controls
+                    className="w-full rounded-t-lg"
+                  >
+                    <source src="./videos/peter.mp4" type="video/mp4" />
+                    <source src="movie.ogg" type="video/ogg" />
+                    Your browser does not support the video tag.
+                  </video>
+                </a>
+                <div className="p-6 text-brown-800">
+                  <h5 className="text-xl font-medium mb-2">Video Card 5</h5>
+                  <p className="text-base mb-2">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
         </div>
+
+        {!showMore && (
+          <div className="flex justify-center">
+            <button
+              className="bg-brown-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8"
+              onClick={handleShowMore}
+            >
+              Show More
+            </button>
+          </div>
+        )}
+
+        {showMore && (
+          <div className="flex justify-center">
+            <button
+              className="bg-brown-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8"
+              onClick={handleShowLess}
+            >
+              Show Less
+            </button>
+          </div>
+        )}
       </div>
     </>
   );
